3854066fed03fefa7119cf2d8f4843a8c35153b6
Kal <kalwardin256@gmail.com>
2025-09-10 12:17:32 +0530

    Task 5 completed

---
+++ Experiment_1_Task_5.py
@@ -0,0 +1,8 @@
+temp_f=int(input("Enter the temperature in fahrenheit:"))
+temp_c=int(input("Enter the temperature in celsius:"))
+temp_F=(temp_c * 9/5) + 32
+temp_C=(temp_f-32) * 5/9
+print(f"Temperature in fahrenheit:{temp_F}")
+print(f"Temperature in celsius:{temp_C}")
+
+
