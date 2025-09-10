32615f180459a87459e35024e10a7223d73826df
Kal <kalwardin256@gmail.com>
2025-09-10 12:10:40 +0530

    Task 4 completed

---
+++ Experiment_1_Task_4.py
@@ -0,0 +1,8 @@
+num1=input("Enter the first complex number:")
+num2=input("Enter the second complex number:")
+cnum1=complex(num1)
+cnum2=complex(num2)
+sum=cnum1+cnum2
+difference=cnum1-cnum2
+product=cnum1*cnum2
+print(sum,difference,product)
